2e566125ce0fbac481ec5421c5855921bebc4fa2
Đỗ Tuấn Anh <tuananhddo@gmail.com>
2020-04-27 21:58:49 +0700

    Test

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var modelsRouter = require('./routes/models');
+var syncRouter = require('./routes/sync');
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/models',modelsRouter);
+app.use('/sync',syncRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/models.js
+++ routes/models.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var fs = require('fs');
+let files;
 
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
   const data = [{name : 'model1'}, {name : 'models2'}];
+  res.send(JSON.stringify(data))
+});
+const BASE_URL = 'http://localhost:3000';
+const BASE_MODEL_URL = BASE_URL + '/models';
+router.post('/',upload.single('avatar'), function(req, res, next) {
+  // res.send('respond with a resource');
+  const processedFile = req.file || {}; // MULTER xử lý và gắn đối tượng FILE vào req
+  let orgName = processedFile.originalname || ''; // Tên gốc trong máy tính của người upload
+  orgName = orgName.trim().replace(/ /g, "-")
+  const fullPathInServ = processedFile.path; // Đường dẫn đầy đủ của file vừa đc upload lên server
+  // Đổi tên của file vừa upload lên, vì multer đang đặt default ko có đuôi file
+  const newFullPath = `${fullPathInServ}-${orgName}`;
+  fs.renameSync(fullPathInServ, newFullPath);
+  const data = [{name : BASE_MODEL_URL + 'floor.glb'}, {name : 'models2'}];
   res.send(JSON.stringify(data))
 });
 
